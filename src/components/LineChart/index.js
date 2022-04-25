eca8d32dff1033484fba3c945cb9fe555f4a7301
sathidporn <77266865+sathidporn@users.noreply.github.com>
2022-04-25 11:45:20 +0700

    Add files

---
+++ src/components/LineChart/index.js
@@ -0,0 +1,31 @@
+import { EchartsComponent } from "echarts-react-wrapper"
+import { useState } from "react";
+
+export default function LineChart({style}){
+    const [echarts, setEcharts] = useState()
+    const option = {
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            data: [150, 230, 224, 218, 135, 147, 260],
+            type: 'line'
+          }
+        ]
+    };
+
+    return(
+        <EchartsComponent
+            onInit={(instance) => setEcharts(instance)}
+            style={style} 
+            option={option}
+            notMerge={true}
+            // eventsHandler={eventHandler}
+        />
+    )
+}
