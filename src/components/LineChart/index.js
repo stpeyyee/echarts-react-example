d23e2b8edc829720724e11c6be7b46f6ef5967cb
sathidporn <77266865+sathidporn@users.noreply.github.com>
2022-06-20 11:45:44 +0700

    Add line X options

--- src/components/LineChart/index.js
+++ src/components/LineChart/index.js
@@ -1,35 +1,68 @@
-import { EchartsComponent } from "echarts-react-wrapper"
-import { useState } from "react";
+import { EchartsComponent } from "echarts-react-wrapper";
+import { useState, useMemo } from "react";
+import debounce from "lodash.debounce";
+export default function LineChart({ style }) {
+  const [echarts, setEcharts] = useState();
+  const option = {
+    brush: {
+      toolbox: ["lineX"],
+      type: "lineX",
+    },
+    xAxis: {
+      type: "category",
+      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        data: [150, 230, 224, 218, 135, 147, 260],
+        type: "line",
+      },
+    ],
+  };
 
-export default function LineChart({style}){
-    const [echarts, setEcharts] = useState()
-    const option = {
-        xAxis: {
-          type: 'category',
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-        },
-        yAxis: {
-          type: 'value'
-        },
-        series: [
-          {
-            data: [150, 230, 224, 218, 135, 147, 260],
-            type: 'line'
-          }
-        ]
-    };
+  let updateMousemove = useMemo(
+    () =>
+      debounce((params) => {
+        console.log("updateMousemove", params);
+      }, 100),
+    []
+  );
 
-    let updateMousemove = (params) => {
-      console.log("updateMousemove",params)
-    }
+  let updateBrushSelected = useMemo(
+    () =>
+      debounce((params) => {
+        console.log("updateBrushSelected", params);
+      }, 100),
+    []
+  );
 
-    return(
-        <EchartsComponent
-            onInit={(instance) => setEcharts(instance)}
-            style={style} 
-            option={option}
-            notMerge={true}
-            eventsHandler={{ 'mousemove': updateMousemove}}
-        />
-    )
+  let updateGlobalCursorTaken = useMemo(
+    () =>
+      debounce((params) => {
+        console.log("updateGlobalCursorTaken", params);
+      }, 100),
+    []
+  );
+
+  const eventHandler = useMemo(
+    () => ({
+      // mouseMove: updateMousemove,
+      brushSelected: updateBrushSelected,
+      globalCursorTaken: updateGlobalCursorTaken,
+    }),
+    [updateBrushSelected, updateGlobalCursorTaken]
+  );
+
+  return (
+    <EchartsComponent
+      onInit={(instance) => setEcharts(instance)}
+      style={style}
+      option={option}
+      notMerge={true}
+      eventsHandler={eventHandler}
+    />
+  );
 }
